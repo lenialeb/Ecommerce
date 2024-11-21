f6d66823a2f58a20aba08bfc20e7b567dc9fb438
biruk-ay <birukindustry@gmail.com>
2024-11-21 17:04:31 +0300

    Updated AuthProvider-provideUserModel

--- src/auth/di/AuthProvider.ts
+++ src/auth/di/AuthProvider.ts
@@ -1,13 +1,20 @@
 import UserRepository from "../repository/UserRepository.js";
-
+import { IUser, UserModel } from "../model/User.js";
 export default class AuthProvider {
 
     private static UserRepository: UserRepository | null = null;
+    private static UserModel: IUser | null = null;
 
     public static provideUserRepository() {
         if (this.UserRepository === null) {
             this.UserRepository = new UserRepository();
         }
         return this.UserRepository;
+    }
+
+    public static provideUserModel() {
+        if (this.UserModel === null) {
+            this.UserModel = new UserModel();
+        }
     }
 }
